c8309501fe842454a0a5589a1823d97f9b864ce9
desmoris <damianesmoris@gmail.com>
2024-02-15 21:50:56 -0300

    Revert "[quickfix] Main index script restored"
    This reverts commit eb35eb448bf9bc0daf56874206c322fef3635b35.

--- public/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import { createCalendar } from "./modules/calendar.js";
-import { setupQuickmenu } from "./modules/quickMenu.js";
-const CALENDAR = document.getElementById('calendarTable');
-const EXPENSES_POPUP = document.getElementById('expensivePopup');
-createCalendar(CALENDAR, EXPENSES_POPUP);
-
-
-setupQuickmenu();
